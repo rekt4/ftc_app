cf9b52a342ba69cc484ffe09d954b1cc5f713e7d
Nikhil Mathihalli <nikhil@mathihalli.com>
2019-10-18 07:03:28 -0700

    Cleaning the code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base_Spool_Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base_Spool_Claw.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name="Base_Spool_Claw", group="boundless")
@@ -9,51 +10,59 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class Base_Spool_Claw extends LinearOpMode{
 
     //initiating motors
-    private DcMotor spool;
-    private DcMotor frontLeft;
-    private DcMotor frontRight;
-    private DcMotor claw;
-    //private DcMotor backLeft;
-    //private DcMotor backRight;
-    //private DcMotor m1,m2,m3,m4; //these motors will be used for other things
+    private DcMotor spool = null;
+    private DcMotor frontLeft = null;
+    private DcMotor frontRight = null;
+    private DcMotor leftRight = null;
+    private Servo claw = null;
+    private double clawPosition = 0.5;
+
     public void runOpMode(){
 
-        //Connecting to the hardware
+        // This connects the software to the hardware
         frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight  = hardwareMap.get(DcMotor.class, "frontRight");
         spool  = hardwareMap.get(DcMotor.class, "spool");
-        claw  = hardwareMap.get(DcMotor.class, "claw");
-        //backLeft  = hardwareMap.get(DcMotor.class, "backLeft");
-        //backRight  = hardwareMap.get(DcMotor.class, "backRight");
+        claw  = hardwareMap.get(Servo.class, "claw");
+        leftRight = hardwareMap.get(DcMotor.class, "leftRight");
 
-        //Updating Telemetry
+
+        // This always updates telemetry
         telemetry.addData("Status", "Initialized");
         telemetry.update();
         waitForStart();
 
-        //Joystick power
+        // This is sets the original joystick power
         double spoolLeftStick=0;
-        double clawRightStick=0;
         double fbLeftStick=0;
-        double lrLeftStick=0;
-        double lrRightStick=0;
         double fbRightStick=0;
+        double leftRightReqPower=0;
         frontRight.setDirection(DcMotor.Direction.REVERSE);
-        //backRight.setDirection(DcMotor.Direction.REVERSE);
 
-        //put all movement code in here
         while(opModeIsActive()){
-            clawRightStick = -this.gamepad2.right_stick_y;
+
+            // This will set the the motors to different parts of the controller.
+            leftRightReqPower=this.gamepad2.right_stick_x;
             spoolLeftStick = -this.gamepad2.left_stick_y;
             fbLeftStick = -this.gamepad1.left_stick_y;
             fbRightStick = -this.gamepad1.right_stick_y;
-            lrLeftStick = -this.gamepad1.left_stick_x; //if turning the wrong direction reverse the sign
-            lrRightStick = -this.gamepad1.right_stick_x; //if turning the wrong direction reverse the sign
 
+            // Using methods created, the spool and the z-movement claw part will be 'activated'
             setPowerToSpool(spoolLeftStick);
-            setPowerToClaw(clawRightStick);
+            setPowerToLeftRight(leftRightReqPower);
 
+            // We first need to stop the claw servo from moving if it is moving
+            claw.setPosition(0.5);
+
+            // Using other methods, we also 'activate' the actual claw mechanism
+            stopClaw();
+            openClaw();
+            closeClaw();
+
+            // Finally the tank is also 'activated'
             tank(fbLeftStick, fbRightStick);
+
+            // Code below helps for debugging
             telemetry.addData("Status", "Running");
             telemetry.update();
         }
@@ -62,66 +71,48 @@ public class Base_Spool_Claw extends LinearOpMode{
 
     public void tank(double fbLeftStick, double fbRightStick){
         frontLeft.setPower(fbLeftStick);
-        //backLeft.setPower(fbLeftStick);
-        //backRight.setPower(fbRightStick);
         frontRight.setPower(fbRightStick);
         telemetry.addData("Target Power", fbLeftStick);
         telemetry.addData("Target Power", fbRightStick);
         telemetry.addData("Motor Power", frontLeft.getPower());
         telemetry.addData("Motor Power", frontRight.getPower());
-        //telemetry.addData("Motor Power", backLeft.getPower());//hi test
-        //telemetry.addData("Motor Power", backRight.getPower());
 
     }
 
-    public void movingFB(double fbLeftStick){
-
-        frontLeft.setPower(fbLeftStick);
-        frontRight.setPower(fbLeftStick);
-        //backLeft.setPower(fbLeftStick);
-        //backLeft.setPower(fbLeftStick);
-        telemetry.addData("Target Power", fbLeftStick);
-        telemetry.addData("Motor Power", frontLeft.getPower());
-        telemetry.addData("Motor Power", frontRight.getPower());
-        //telemetry.addData("Motor Power", backLeft.getPower());
-        //telemetry.addData("Motor Power", backRight.getPower());
-    }
-
-    public void strafe(double lrLeftStick){
-
-        frontLeft.setPower(lrLeftStick);
-        //backLeft.setPower(-lrLeftStick);
-        frontRight.setPower(-lrLeftStick);
-        //backRight.setPower(lrLeftStick);
-        telemetry.addData("Target Power", lrLeftStick);
-        telemetry.addData("Motor Power", frontLeft.getPower());
-        telemetry.addData("Motor Power", frontRight.getPower());
-        //telemetry.addData("Motor Power", backLeft.getPower());//hi test
-        //telemetry.addData("Motor Power", backRight.getPower());
-    }
-
-    public void turn(double lrRightStick){
-
-        frontLeft.setPower(lrRightStick);
-        //backLeft.setPower(lrRightStick);
-        frontRight.setPower(-lrRightStick);
-        //backRight.setPower(-lrRightStick);
-        telemetry.addData("Target Power", lrRightStick);
-        telemetry.addData("Motor Power", frontLeft.getPower());
-        telemetry.addData("Motor Power", frontRight.getPower());
-        //telemetry.addData("Motor Power", backLeft.getPower());
-        //telemetry.addData("Motor Power", backRight.getPower());
-    }
-
-    public void setPowerToSpool(double fbLeftStick){
-        spool.setPower(fbLeftStick);
-        telemetry.addData("Target Power", fbLeftStick);
+    public void setPowerToSpool(double Stick){
+        spool.setPower(Stick);
+        telemetry.addData("Target Power", Stick);
 
     }
 
-    public void setPowerToClaw(double fbLeftStick){
-        claw.setPower(fbLeftStick);
-        telemetry.addData("Target Power", fbLeftStick);
+    public void openClaw() {
+        if (gamepad2.y) {
+            clawPosition = 0.9;
+        }
+        claw.setPosition(clawPosition);
+        telemetry.addData("Claw Power", clawPosition);
+    }
+
+    public void closeClaw() {
+        if (gamepad2.a) {
+            clawPosition = 0.1;
+        }
+        claw.setPosition(clawPosition);
+        telemetry.addData("Claw Power", clawPosition);
+    }
+
+    public void stopClaw() {
+        if(gamepad2.b) {
+            clawPosition = 0.5;
+        }
+        claw.setPosition(clawPosition);
+        telemetry.addData("Claw Power", clawPosition);
+    }
+
+
+    public void setPowerToLeftRight(double power) {
+        leftRight.setPower(-power);
+        telemetry.addData("Target Power", power);
     }
 
 }
