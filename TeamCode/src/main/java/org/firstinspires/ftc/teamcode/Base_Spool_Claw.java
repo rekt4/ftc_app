b22f289ad75f2e50997121ee9d25d8f1fd71f5a7
Nikhil Mathihalli <nikhil@mathihalli.com>
2019-10-19 10:28:43 -0700

    Cleanup comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base_Spool_Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base_Spool_Claw.java
@@ -10,21 +10,30 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 public class Base_Spool_Claw extends LinearOpMode{
 
-    //initiating motors
+    // Initiate all the motors. We have 5 motors
+
+    // The spool controls the up/down motion
     private DcMotor spool = null;
+
+    // The frontLeft and frontRight controls the drive train
     private DcMotor frontLeft = null;
     private DcMotor frontRight = null;
+
+    // The leftRight controls the horizontal motion
     private DcMotor leftRight = null;
+
+    // The claw controls the font-back claw
     private Servo claw = null;
+    // A position of 0.5 means the claw stops
     private double clawPosition = 0.5;
 
     public void runOpMode(){
 
         // This connects the software to the hardware
-        frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
-        frontRight  = hardwareMap.get(DcMotor.class, "frontRight");
-        spool  = hardwareMap.get(DcMotor.class, "spool");
-        claw  = hardwareMap.get(Servo.class, "claw");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        spool = hardwareMap.get(DcMotor.class, "spool");
+        claw = hardwareMap.get(Servo.class, "claw");
         leftRight = hardwareMap.get(DcMotor.class, "leftRight");
 
 
@@ -41,15 +50,11 @@ public class Base_Spool_Claw extends LinearOpMode{
         frontRight.setDirection(DcMotor.Direction.REVERSE);
 
         while(opModeIsActive()){
-
-            // This will set the the motors to different parts of the controller.
-            leftRightReqPower=this.gamepad2.right_stick_x;
-            spoolLeftStick = -this.gamepad2.left_stick_y;
-            fbLeftStick = -this.gamepad1.left_stick_y;
-            fbRightStick = -this.gamepad1.right_stick_y;
-
             // Using methods created, the spool and the z-movement claw part will be 'activated'
+            spoolLeftStick = -this.gamepad2.left_stick_y;
             setPowerToSpool(spoolLeftStick);
+
+            leftRightReqPower = this.gamepad2.right_stick_x;
             setPowerToLeftRight(leftRightReqPower);
 
             // We first need to stop the claw servo from moving if it is moving
@@ -61,6 +66,8 @@ public class Base_Spool_Claw extends LinearOpMode{
             closeClaw();
 
             // Finally the tank is also 'activated'
+            fbLeftStick = -this.gamepad1.left_stick_y;
+            fbRightStick = -this.gamepad1.right_stick_y;
             tank(fbLeftStick, fbRightStick);
 
             // Code below helps for debugging
@@ -73,16 +80,14 @@ public class Base_Spool_Claw extends LinearOpMode{
     public void tank(double fbLeftStick, double fbRightStick){
         frontLeft.setPower(fbLeftStick);
         frontRight.setPower(fbRightStick);
-        telemetry.addData("Target Power", fbLeftStick);
-        telemetry.addData("Target Power", fbRightStick);
-        telemetry.addData("Motor Power", frontLeft.getPower());
-        telemetry.addData("Motor Power", frontRight.getPower());
+        telemetry.addData("Motor Power - Left", frontLeft.getPower());
+        telemetry.addData("Motor Power - Right", frontRight.getPower());
 
     }
 
     public void setPowerToSpool(double Stick){
         spool.setPower(Stick);
-        telemetry.addData("Target Power", Stick);
+        telemetry.addData("Up/Down Power", Stick);
 
     }
 
@@ -113,6 +118,6 @@ public class Base_Spool_Claw extends LinearOpMode{
 
     public void setPowerToLeftRight(double power) {
         leftRight.setPower(-power);
-        telemetry.addData("Target Power", power);
+        telemetry.addData("Left/Right Power", power);
     }
 }
